197576568c0fa546ecf1228ba083ed840c27f041
Michael Oduah <oduahmichaelebuka@gmail.com>
2023-07-28 06:00:32 +0100

    added letter search routes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
-  home
+    <div class="flex p-8 justify-center">
+        <input type="text" class="rounded border-2 border-grey-200 w-full" placeholder="Search For Meals"/>
+    </div>
+
+    <div class="flex gap-1">
+        <router-link to="/" v-for="letter of letters">
+            {{ letter }}
+        </router-link>
+    </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import store from '../store'
+
+
+const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
 
 </script>
 
